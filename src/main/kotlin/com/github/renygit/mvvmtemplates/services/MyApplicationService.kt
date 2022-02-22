9608f5d5a789df88b3922e39604e105aa0c39b14
GitHub Action <action@github.com>
2022-02-22 07:15:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/renygit/mvvmtemplates/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/renygit/mvvmtemplates/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.renygit.mvvmtemplates.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.renygit.mvvmtemplates.MyBundle
 
 class MyApplicationService {
 
